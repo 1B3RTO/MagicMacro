cbd2b67e6890bfe98cad9b075a61f738fefcc5b2
Umberto <umberto.messuti@mail.polimi.it>
2023-02-15 21:55:03 +0100

    Rewrite display_handler.py, First implementation of macro_board_handler.py

--- magic_macro/display_handler/display_handler.py
+++ magic_macro/display_handler/display_handler.py
@@ -1,5 +1,108 @@
-class DisplayHandler:
-    def __init__(self, macropad):
-        self._macropad = macropad
+from displayio import Group
+from adafruit_macropad import MacroPad
 
-    pass
+import displayio
+import terminalio
+from adafruit_display_shapes.rect import Rect
+from adafruit_display_text import label
+from magic_macro.config import DEFAULT_DISPLAY_WIDTH, DEFAULT_DISPLAY_HEIGHT, CENTRAL_ANCHOR_POINT
+
+
+class DisplayHandler:
+    def __init__(self, macropad: MacroPad):
+        self._macropad: MacroPad = macropad
+        self._inside_macro_group: Group = Group()
+        self._macro_selector_group: Group = Group()
+        self.__initialize_groups()
+
+    def __initialize_groups(self):
+        # Initiate _inside_macro_group
+        for key_index in range(12):
+            x = key_index % 3
+            y = key_index // 3
+            self._inside_macro_group.append(label.Label(terminalio.FONT, text='EMPTY', color=0xFFFFFF,
+                                                        anchored_position=((DEFAULT_DISPLAY_WIDTH - 1) * x / 2,
+                                                                           DEFAULT_DISPLAY_HEIGHT - 1 -
+                                                                           (3 - y) * 12),
+                                                        anchor_point=(x / 2, 1.0)))
+        self._inside_macro_group.append(Rect(0, 0, DEFAULT_DISPLAY_WIDTH, 12, fill=0xFFFFFF))
+        self._inside_macro_group.append(label.Label(terminalio.FONT, text='TITLE', color=0x000000,
+                                                    anchored_position=(DEFAULT_DISPLAY_WIDTH // 2, 0),
+                                                    anchor_point=(0.5, 0.0)))
+
+        # Initiate macro_selector_group
+        self._macro_selector_group.append(Rect(0, 0, DEFAULT_DISPLAY_WIDTH, 16, outline=0xffffff, stroke=1))
+        for line in range(4):
+            self._macro_selector_group.append(
+                label.Label(
+                    terminalio.FONT,
+                    text=f"Line {line}",
+                    color=0xFFFFFF,
+                    anchored_position=(DEFAULT_DISPLAY_WIDTH // 2, 16 * line),
+                    anchor_point=CENTRAL_ANCHOR_POINT,
+                )
+            )
+
+    def menu_selector(self, encoder_position: int, menu_list: list):
+        """
+        Setup and return a group for the specific menu_list
+        :param encoder_position: the int position of the rotary encoder
+        :param menu_list: list with all the str names of all the boards available
+        :return: the configured Group object
+        """
+        norm_encoder = encoder_position % len(menu_list)
+        selected_cell = norm_encoder % 4
+        starting_index = int(norm_encoder / 4) * 4
+
+        for i in range(4):
+            self._macro_selector_group[i + 1].text = ""
+
+        for i, elem in enumerate(menu_list[starting_index: starting_index + 4]):
+            self._macro_selector_group[i + 1].text = elem
+
+        self._macro_selector_group[0] = Rect(0, selected_cell * 16, DEFAULT_DISPLAY_WIDTH, 16, outline=0xffffff,
+                                             stroke=1)
+
+        self.update_view(self._macropad, self._macro_selector_group)
+
+    def set_inside_macro_view(self, title: str, labels: list[str]):
+        """
+        Setup and return a group for the specific macro board
+        :param title: The name of the board
+        :param labels: an array of 12 str names
+        :return: the configured Group object
+        """
+        assert len(labels) == 12
+
+        self._inside_macro_group[13].text = title
+
+        for i, text in enumerate(labels):
+            self._inside_macro_group[i].text = text
+
+        self.update_view(self._macropad, self._inside_macro_group)
+
+    def select_line(self, line_index: int) -> displayio.Group:
+        """
+        Update the selected line of the menu selector
+        :param line_index: int of the selected line
+        :return: the configured Group object
+        """
+        self._macro_selector_group[0] = Rect(0, line_index * 16, DEFAULT_DISPLAY_WIDTH, 16, outline=0xffffff, stroke=1)
+        return self._macro_selector_group
+
+    def update_macro_selector_line(self, line_index, anchored_position, anchor_point):
+        # Debug method
+        self._macro_selector_group[line_index + 1] = label.Label(
+            terminalio.FONT,
+            text=f"Line {line_index}",
+            color=0xFFFFFF,
+            anchored_position=anchored_position,
+            anchor_point=anchor_point,
+        )
+
+        return self._macro_selector_group
+
+    @staticmethod
+    def update_view(macropad: MacroPad, groups: Group):
+        macropad.display.show(groups)
+        macropad.display.refresh()
--- magic_macro/macro_board_handler/macro_board_handler.py
+++ magic_macro/macro_board_handler/macro_board_handler.py
@@ -1,13 +1,75 @@
-from magic_macro.utils.context import system_context, macro_context
+from adafruit_macropad import MacroPad
+
+from magic_macro.utils.context import context
 from magic_macro.utils.macro_loader import load_macros_from_folder
+from magic_macro.macro_board_handler.macro_board import MacroBoard
+from magic_macro.display_handler.display_handler import DisplayHandler
+from magic_macro.utils.enums import TriggerType, Topics
 
 
 class MacroBoardHandler:
     def __init__(self, macropad):
+        self._macropad: MacroPad = macropad
+        self._display_handler = DisplayHandler(macropad)
+        self._selected_board = 0
+        self._is_selector_view = False
+
         # Load all the macro boards
+        self._macro_boards: list[MacroBoard] = load_macros_from_folder()
+        self._titles: list[str] = self.__get_board_titles()
 
         # Listener: Set menu switch on short press of rotary encoder
+        context.subscribe_single(Topics.BUTTON_PRESS, self.__on_button_press)
+
         # Set menu view
+        self.__switch_view()
+
+    def __get_board_titles(self) -> list[str]:
+        titles = []
+        for board in self._macro_boards:
+            titles.append(board.title)
+        return titles
+
+    def __on_button_press(self, caller_id, trigger_type, timestamp):
+        if caller_id == 12 and trigger_type is TriggerType.ON_SHORT_PRESS:
+            # switch view
+            self.__switch_view()
+
+        elif self._is_selector_view:
+            # menu view operations
+            if caller_id == 13:
+                self.__acw_rotation()
+            elif caller_id == 14:
+                self.__cw_rotation()
+
+        else:
+            # Board view operation
+            # TODO: get action to exec from the board and add it to the queue
+            pass
         pass
 
-    pass
+    def __switch_view(self):
+        self._is_selector_view = not self._is_selector_view
+        if self._is_selector_view:
+            # Set selector view
+            self._display_handler.menu_selector(self._selected_board, self._titles)
+        else:
+            # Setup board view
+            self._selected_board %= len(self._titles)
+            selected_board = self._macro_boards[self._selected_board]
+            print(selected_board.get_names())
+            self._display_handler.set_inside_macro_view(**selected_board.get_names())
+
+    def __acw_rotation(self):
+        # Short press button 13
+        self._selected_board -= 1
+
+        # Update selector view
+        self._display_handler.menu_selector(self._selected_board, self._titles)
+
+    def __cw_rotation(self):
+        # Short press button 14
+        self._selected_board += 1
+
+        # Update selector view
+        self._display_handler.menu_selector(self._selected_board, self._titles)
